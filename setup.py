c4445625b833388701572e58d1f45984fbd31b2d
Shéikh Adnan <manofuranium@gmail.com>
2021-01-15 19:58:03 +0530

    No need of pexpect

--- setup.py
+++
@@ -1,8 +0,0 @@
-import pexpect
-child = pexpect.spawn ('sudo bash setup.sh')
-child.expect ('Enter a PIN of at least six digits: ')
-child.sendline ('556677')
-child.expect ('Enter the same PIN again: ')
-child.sendline ('556677')
-print(child.before)
-child.interact()
